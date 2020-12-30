33106c7329385a62faa8dfe6c96ff2f22d784403
nelsonbarbosa46 <61154147+nelsonbarbosa46@users.noreply.github.com>
2020-12-30 21:21:48 +0000

    Criada função para alterar email/password no cliente/condutor #71

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const personValid = require('../middleware/checkPersonValid');
 
 router.get('/', userController.getUsers);
+
+router.put('/changeEP/:id', personValid, userController.changeEmailPassword)
 
 module.exports = router;

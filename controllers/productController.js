2501d05c1f65d3eb511f7223ffafc36884cb4054
nelsonbarbosa46 <61154147+nelsonbarbosa46@users.noreply.github.com>
2021-01-10 17:36:58 +0000

    #146 #156 Alterado forma de inserir produto e iniciada criação da função de alterar produto
    O que foi alterado no inserir produto:
    Se acontecer algum erro eliminar a imagem (se foi enviada pelo user) e obter o idMerchant através do idUser fazendo um select

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,32 @@
 const jwt = require('jsonwebtoken');
 
+function deleteImage(logo, fs) {
+    //check if logo is uploaded
+    if (logo) {
+        //delete new image because was error on update
+        fs.unlink(logo.path, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+    }
+}
+
 exports.createProduct = (req, res, next) => {
     
     var idUser = req.body.idUser;
-    var idMerchant = req.body.idMerchant;
     var name = req.body.name;
     var price = req.body.price;
     var quantity = req.body.quantity;
     var parseQuantity = parseInt(quantity);
     var description = req.body.description;
     var logo = req.file;
+    const fs = require('fs');
 
     const tokenUnsplited = req.headers.authorization;
     //check if token is empty
     if (!tokenUnsplited) {
+        deleteImage(logo, fs);
         let response = {
             message: "failed",
             request: {
@@ -29,6 +42,7 @@ exports.createProduct = (req, res, next) => {
 
         //check if typeUser is equal to 2 (type Merchant) and if idMerchant is equal to id inside a token
         if (decoded.typeUser != 2 || decoded.id != idUser) {
+            deleteImage(logo, fs);
             let response = {
                 message: "failed",
                 request: {
@@ -40,6 +54,7 @@ exports.createProduct = (req, res, next) => {
             res.status(401).json(response)
         //check if any field is empty
         } else if (!name || !price || !quantity) {
+            deleteImage(logo, fs);
             let response = {
                 message: "failed",
                 request: {
@@ -51,6 +66,7 @@ exports.createProduct = (req, res, next) => {
             res.status(400).json(response)
         //check if quantity is invalid
         } else if (!parseQuantity || quantity < 0) {
+            deleteImage(logo, fs);
             let response = {
                 message: "failed",
                 request: {
@@ -62,6 +78,7 @@ exports.createProduct = (req, res, next) => {
             res.status(400).json(response)
         //check if price is invalid
         } else if (isNaN(price) || price < 0) {
+            deleteImage(logo, fs);
             let response = {
                 message: "failed",
                 request: {
@@ -85,9 +102,10 @@ exports.createProduct = (req, res, next) => {
             }
 
             var db = require('../sql').db();
-            var sql = `SELECT name FROM product WHERE name = ? AND idMerchant = ?`;
-            db.get(sql, [name, idMerchant], function (err, row) {
+            var sql = `SELECT id FROM merchant WHERE idUser = ?`;
+            db.get(sql, [idUser], function (err, row) {
                 if (err) {
+                    deleteImage(logo, fs);
                     let response = {
                         message: "failed",
                         request: {
@@ -99,21 +117,11 @@ exports.createProduct = (req, res, next) => {
                     res.status(500).json(response)
                 } else {
                     if (row) {
-                        let response = {
-                            message: "failed",
-                            typeError: "o nome do produto já existe",
-                            request: {
-                                type: 'POST',
-                                description: 'Criar um produto'
-                            }
-                        }
-                        //product already exists
-                        res.status(500).json(response)
-                    } else {
-                        sql = `INSERT INTO product(idMerchant, name, price, quantity, description, image) VALUES 
-                        (?, ?, ?, ?, ?, ?)`;
-                        db.run(sql, [idMerchant, name, price, quantity, description, logo], function (err) {
+                        var idMerchant = row.id;
+                        sql = `SELECT name FROM product WHERE name = ? AND idMerchant = ?`;
+                        db.get(sql, [name, idMerchant], function (err, row) {
                             if (err) {
+                                deleteImage(logo, fs);
                                 let response = {
                                     message: "failed",
                                     request: {
@@ -121,35 +129,79 @@ exports.createProduct = (req, res, next) => {
                                         description: 'Criar um produto'
                                     }
                                 }
-                                //error inserting
+                                //error selecting
                                 res.status(500).json(response)
                             } else {
-                                let response = {
-                                    message: "success",
-                                    newProduct: {
-                                        name: name,
-                                        price: price,
-                                        quantity: quantity,
-                                        description: description
-                                    },
-                                    request: {
-                                        type: 'POST',
-                                        description: 'Criar uma empresa'
+                                if (row) {
+                                    deleteImage(logo, fs);
+                                    let response = {
+                                        message: "failed",
+                                        typeError: "o nome do produto já existe",
+                                        request: {
+                                            type: 'POST',
+                                            description: 'Criar um produto'
+                                        }
                                     }
+                                    //product already exists
+                                    res.status(500).json(response)
+                                } else {
+                                    sql = `INSERT INTO product(idMerchant, name, price, quantity, description, image) VALUES 
+                                    (?, ?, ?, ?, ?, ?)`;
+                                    db.run(sql, [idMerchant, name, price, quantity, description, logo], function (err) {
+                                        if (err) {
+                                            deleteImage(logo, fs);
+                                            let response = {
+                                                message: "failed",
+                                                request: {
+                                                    type: 'POST',
+                                                    description: 'Criar um produto'
+                                                }
+                                            }
+                                            //error inserting
+                                            res.status(500).json(response)
+                                        } else {
+                                            let response = {
+                                                message: "success",
+                                                newProduct: {
+                                                    name: name,
+                                                    price: price,
+                                                    quantity: quantity,
+                                                    description: description
+                                                },
+                                                request: {
+                                                    type: 'POST',
+                                                    description: 'Criar uma empresa'
+                                                }
+                                            }
+                                            res.status(201).json(response)
+                                        }
+                                    })
                                 }
-                                res.status(201).json(response)
                             }
-                        })
+                        });  
+                    } else {  
+                        deleteImage(logo, fs);          
+                        let response = {
+                            message: "failed",
+                            request: {
+                                type: 'POST',
+                                description: 'Criar um produto'
+                            }
+                        }
+                        //dont find merchant
+                        res.status(500).json(response)
                     }
                 }
             });
-
+            
             db.close();
         }
     }
+    return;
+}
 
-
+exports.changeInfoProduct = (req, res, next) => {
     
-
+    
     return;
 }
--- middleware/checkPersonValid.js
+++ middleware/checkPersonValid.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        
-        const token = req.headers.authorization.split(' ')[1];
-        var decoded = jwt.verify(token, process.env.PRIVATE_KEY);
-        if (decoded.id == req.params.id) {
-            next();
-        } else {
+        const tokenUnsplited = req.headers.authorization;
+        if (!tokenUnsplited) {
             throw "error";
+        } else {
+            const token = req.headers.authorization.split(' ')[1];
+            var decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+            if (decoded.id == req.params.id) {
+                next();
+            } else {
+                throw "error";
+            }
         }
     } catch (err) {
         let response = {

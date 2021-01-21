a750941a7c3f1bf4a9eb09fe4c3fd6ce76d9b079
nelsonbarbosa46 <61154147+nelsonbarbosa46@users.noreply.github.com>
2021-01-21 21:22:16 +0000

    Corrigido erro de sqlite locked

--- sql.js
+++ sql.js
@@ -1,11 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 
 exports.db = function () {
-    return new sqlite3.Database('./db/infood.db', (err) => {
+    let db = new sqlite3.Database('./db/infood.db', (err) => {
         if (err) {
             return console.log(err.message);
         }
     
         console.log('Connected to SQLite');
     });
+    db.run('PRAGMA busy_timeout = 6000');
+    db.configure("busyTimeout", 6000);
+    return db;
 };

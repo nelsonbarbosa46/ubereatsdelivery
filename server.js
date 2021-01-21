b6c1f27139700ed284c7c7db1b48171c454735de
nelsonbarbosa46 <61154147+nelsonbarbosa46@users.noreply.github.com>
2021-01-21 21:11:29 +0000

    Correção de uns erros código para docker funcionar

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
-const app = require('.');
-const port = process.env.PORT || 3000;
+const app = require('./app');
+const port = process.env.PORT || 8080;
+const host = '0.0.0.0';
 const server = http.createServer(app);
 
 console.log(port);
 
-server.listen(port);
+server.listen(port, host);
